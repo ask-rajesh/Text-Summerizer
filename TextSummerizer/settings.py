9f4cfe8e854e885750e89fec644e1ffa8a5b0426
Rajesh <ask.rajesh.1008@gmail.com>
2022-07-19 13:19:38 +0530

    set2

--- TextSummerizer/settings.py
+++ TextSummerizer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #-------------------added Manually----------
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
